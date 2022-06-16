125657a866f0d4ab2c441ca0c2b052b494425f3f
Jisoo Choi <jschoi.py@gmail.com>
2022-06-16 18:55:54 +0900

    Design quotes.js & background.js

---
+++ js/quotes.js
@@ -0,0 +1,51 @@
+const quotes = [
+    {
+        quote: "It is during our darkest moments that we must focus to see the light.",
+        author: "Aristotle",
+    },
+    {
+        quote: "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+        author: "Nelson Mandela",
+    },
+    {
+        quote: "The way to get started is to quit talking and begin doing.",
+        author: "Walt Disney",
+    },
+    {
+        quote: "Life is what happens when you're busy making other plans.",
+        author: "John Lennon",
+    },
+    {
+        quote: "The future belongs to those who believe in the beauty of their dreams.",
+        author: "Eleanor Roosevelt",
+    },
+    {
+        quote: "Whoever is happy will make others happy too.",
+        author: "Anne Frank",
+    },
+    {
+        quote: "Don't judge each day by the harvest you reap but by the seeds that you plant.",
+        author: "Robert Louis Stevenson",
+    },
+    {
+        quote: "You will face many defeats in life, but never let yourself be defeated.",
+        author: "Maya Angelou",
+    },
+    {
+        quote: "Life is either a daring adventure or nothing at all.",
+        author: "Helen Keller",
+    },
+    {
+        quote: "The only impossible journey is the one you never begin.",
+        author: "Tony Robbins",
+    },
+
+];
+
+const quote = document.querySelector("#quote span:first-child");
+const author = document.querySelector("#quote span:last-child");
+
+const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+quote.innerText = todaysQuote.quote;
+author.innerText = todaysQuote.author;
